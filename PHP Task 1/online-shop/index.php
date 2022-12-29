6e325b645df7d6d886d65119de6fa08ac9bddf37
Khaled Mohamed <khaledm99a@gmail.com>
2022-12-29 20:49:04 +0200

    PHP Task 1 Done

--- PHP Task 1/online-shop/index.php
+++ PHP Task 1/online-shop/index.php
@@ -124,7 +124,7 @@ $categories = getCategories();
 <!-- Categories Start -->
 <div class="container-fluid pt-5">
     <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4">
-        <span class="bg-secondary pr-3">Categories<!--testy1--></span>
+        <span class="bg-secondary pr-3">Categories</span>
     </h2>
     <div class="row px-xl-5 pb-3">
         <?php
