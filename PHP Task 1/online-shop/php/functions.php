838e9df05817b96e63fbc8dc1b72a00f7638b6f8
Khaled Mohamed <khaledm99a@gmail.com>
2022-12-29 21:26:28 +0200

    PHP Task 1 Done After Comments

--- PHP Task 1/online-shop/php/functions.php
+++ PHP Task 1/online-shop/php/functions.php
@@ -1,11 +1,13 @@
 <?php
 function calculateStars($num){
     $string = '';
-    for($i = 0; $i < (int)$num; $i++){
-        $string .= '<small class="fa fa-star text-primary mr-1"></small>';
-    }
-    if(($num - (int)$num) > 0){
-        $string .= '<small class="fa fa-star-half-alt text-primary mr-1"></small>';
+    for($i = 0; $i < 5; $i++){
+        if($num - $i == 0.5)
+            $string .= '<small class="fa fa-star-half-alt text-primary mr-1"></small>';
+        elseif($i < $num)
+            $string .= '<small class="fa fa-star text-primary mr-1"></small>';
+        else
+            $string .= '<small class="far fa-star text-primary mr-1"></small>';
     }
 
     return $string;
