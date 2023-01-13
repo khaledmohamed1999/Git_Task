d720fcec73b9f64509248ddce35c282f09a25de9
Khaled Mohamed <khaledm99a@gmail.com>
2023-01-13 13:28:19 +0200

    Laravel Task 2 Done

---
+++ Laravel Task 2/ProductsController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view('admin.products.index')->with('products', Product::paginate(10));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        $categories = Category::all();
+        return view('admin.products.create',compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate(Product::$rules);
+        //dd($request->post());
+        $imageUrl = $request->file('image')->store('products', ['disk' => 'public']);
+        $product = new Product;
+        $product->fill($request->post());
+        $product['image'] = $imageUrl;
+        $product['rating'] = 0;
+        $product['rating_count'] = 0;
+        $product -> save();
+        return redirect()->route('admin.products');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+        return view('admin.products.show', compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $categories = Category::all();
+        $product = Product::findOrFail($id);
+        return view('admin.products.edit', compact('product','categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $request->validate(Product::$rules);
+
+        $product->fill($request->post());
+
+        $imageUrl = $request->file('image')->store('products', ['disk' => 'public']);
+
+        $product['image'] = $imageUrl;
+
+        $product->save();
+        return redirect()->route('admin.products');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+        Product::destroy($id);
+        return redirect()->route('admin.products')->with('success', 'Record has been deleted successfully!');
+        
+    }
+}
